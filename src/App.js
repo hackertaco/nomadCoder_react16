947059e5c0d09cb5886e8ab7e2af62ed39b38ab0
승가리 <imsa1224@korea.ac.kr>
2020-03-03 03:15:54 +0900

    set state null

--- src/App.js
+++ src/App.js
@@ -1,13 +1,34 @@
-import React from "react";
+import React, { useState, useEffect, usePromise } from "react";
 import { createPortal } from "react-dom";
-function Portals() {
-  return createPortal(<Ms />, document.getElementById("touchMe"));
+const MAX_PIZZAS = 20;
+
+function Controlled() {
+  let [pizza, setPizza] = useState(10);
+  function eatPizza() {
+    if (pizza < MAX_PIZZAS) {
+      return (pizza += 1);
+    } else {
+      return null;
+    }
+  }
+
+  function handle() {
+    setPizza(eatPizza);
+  }
+
+  return (
+    <button onClick={handle}>
+      I've eaten {pizza} {pizza === 1 ? "pizza" : "pizzas"}
+    </button>
+  );
 }
-const Ms = () => "just touched this";
+
 function App() {
   return (
     <div className="App">
-      <Portals />
+      {/* <Portals />
+      <ErrorMaker /> */}
+      <Controlled />
     </div>
   );
 }
